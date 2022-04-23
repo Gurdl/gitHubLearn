070c9f982378cebfd1536594895184e7b55f1c19
Gurdev singh <Gurdevsingh95801@gmail.com>
2022-04-23 13:04:35 -0700

    after modifying ,file one

--- LeanGit.java
+++ LeanGit.java
@@ -2,6 +2,7 @@ public class LeanGit{
     public static void main(String[] args) 
     {
         System.out.println("The sum of two numbers which are 7 and 8 is "+ (7+8));
+        System.out.println("The sum of two numbers which are 7 and 8 is "+ (7+8));
     }
 }
 
