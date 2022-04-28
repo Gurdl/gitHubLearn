c5e53092fbb4817eec05b23dab0487e201ece294
Gurdev singh <Gurdevsingh95801@gmail.com>
2022-04-28 11:35:10 -0700

    again ,first branch is commited

---
+++ LeanGit.java
@@ -0,0 +1,11 @@
+
+public class LeanGit{
+    public static void main(String[] args) 
+    {
+        System.out.println("can i write here?");
+        System.out.println("well done you started commiting changes in files");
+        System.out.println("can i write here?");
+        System.out.println("Now I just created a first branch named FIRST BRANCH IN GIT ");
+    }
+}
+
