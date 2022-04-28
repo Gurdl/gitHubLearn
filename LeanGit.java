18ba02956f837cecb974cc07faca808b7592dad9
Gurdev singh <Gurdevsingh95801@gmail.com>
2022-04-28 11:33:00 -0700

    first branch craeted and printed

--- LeanGit.java
+++
@@ -1,9 +0,0 @@
-
-public class LeanGit{
-    public static void main(String[] args) 
-    {
-        System.out.println("can i write here?");
-        System.out.println("well done you started commiting changes in files");
-    }
-}
-
